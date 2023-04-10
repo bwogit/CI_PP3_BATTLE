7641e1cfa624432cc4556b9f76e4083a2b022f9b
bwogit <jeremie.sandot@hotmail.com>
2023-04-10 17:50:51 +0000

    creating Board class

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-import random
+from random import randint
 
 class Board:
+    """
+    Main board class.
+    """
+
+    def __init__(self, size, player_name, board_type):
+        """
+        initializing the grid with '.' characters for each tile
+        """
+        self.size = size
+        self.grid = [['.' for _ in range(size)] for _ in range(size)]
+        self.ships = []
+        self.player_name = player_name
+        self.board_type = board_type
