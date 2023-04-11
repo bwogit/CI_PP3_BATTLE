4945f932b74b76189584d1e8c1345da51a0325ab
bwogit <jeremie.sandot@hotmail.com>
2023-04-11 13:36:40 +0000

    Sheet Variable created

--- run.py
+++ run.py
@@ -11,6 +11,7 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('Battleship')
 
 # Load the dictionary from Google Sheets
 sheet = client.open('Battleship').auth
