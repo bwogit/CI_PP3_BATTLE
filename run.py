d29eecfb0c286c017d2a5dbd0a19fb69cb52647c
bwogit <jeremie.sandot@hotmail.com>
2023-04-11 16:40:08 +0000

    Fixing indentation errors

--- run.py
+++ run.py
@@ -2,23 +2,24 @@ import random
 import gspread
 from google.oauth2.service_account import Credentials
 
+
 def authenticate():
 
-# Set up Google Sheets credentials
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('Battleship')
+    # Set up Google Sheets credentials
+    SCOPE = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/drive"
+        ]
+    CREDS = Credentials.from_service_account_file('creds.json')
+    SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+    GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+    SHEET = GSPREAD_CLIENT.open('Battleship')
 
-# Get the auth_dict worksheet
+    # Get the auth_dict worksheet
 
-auth_dict_worksheet = SHEET.worksheet('auth_dict')
-return auth_dict_worksheet
+    auth_dict_worksheet = SHEET.worksheet('auth_dict')
+    return auth_dict_worksheet
 
 
 def login(username, password):
