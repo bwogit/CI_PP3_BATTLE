1f8a8fd4d2ed181a59be7b61a82d2aa7821b661a
bwogit <jeremie.sandot@hotmail.com>
2023-04-11 13:31:49 +0000

    Fixing typos Credentials instead of credentials

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import random
 import gspread
-from google.oauth2.service_account import credentials
+from google.oauth2.service_account import Credentials
 
 # Set up Google Sheets credentials
 SCOPE = [
